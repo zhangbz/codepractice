ae66ffe7dca28b3efcb6f40f2e0df77a335e197b
zhangbz <zhangbz.happyhacking@gmail.com>
2019-09-02 22:30:25 +0800

    07 斐波那契数列

---
+++ src/main/java/Solution06.java
@@ -0,0 +1,16 @@
+/**
+ * 斐波那契数列
+ *
+ * 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+ * n<=39
+ */
+public class Solution06 {
+    public int Fibonacci(int n) {
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
